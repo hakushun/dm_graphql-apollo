36f06ea40d2891da835a61004da0a51546f1cbcb
hakushun <hakushun.pianist@gmail.com>
2021-12-19 13:44:43 +0900

    feat: add create function on todo resolver

--- server/src/todo/todo.resolver.ts
+++ server/src/todo/todo.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, ID, Query, Resolver } from '@nestjs/graphql';
+import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateTodoDto } from './dto/create-todo.dto';
 import { Todo } from './models/todo.models';
 import { TodoService } from './todo.service';
 
@@ -14,5 +15,10 @@ export class TodoResolver {
   @Query(() => Todo)
   findOneById(@Args('id', { type: () => ID }) id: string) {
     return this.todoService.findOneById(id);
+  }
+
+  @Mutation((returns) => Todo)
+  create(@Args('todo') todo: CreateTodoDto): Todo {
+    return this.todoService.create(todo);
   }
 }
