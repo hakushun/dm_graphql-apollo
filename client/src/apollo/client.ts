efdd1afe1146939ef9f2c7eae83d00a1c47aeabd
hakushun <hakushun.pianist@gmail.com>
2021-12-19 19:35:17 +0900

    chore: lint fix

--- client/src/apollo/client.ts
+++ client/src/apollo/client.ts
@@ -1,9 +1,4 @@
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  NormalizedCacheObject,
-} from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
 import { useMemo } from "react";
 
 const httpLink = new HttpLink({
