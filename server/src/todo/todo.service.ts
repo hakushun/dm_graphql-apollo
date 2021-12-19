f9a57f680f6347180dfbf926ba9bb10e77b1a7df
hakushun <hakushun.pianist@gmail.com>
2021-12-19 13:57:29 +0900

    feat: add updateStatus function on todo serveice

--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo, TodoStatus } from './models/todo.models';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
 @Injectable()
 export class TodoService {
@@ -28,6 +29,24 @@ export class TodoService {
       updatedAt: new Date(),
     };
     this.todos.push(newTodo);
+    return newTodo;
+  }
+
+  updateStatus(updateStatusDto: UpdateStatusDto): Todo {
+    const targetTodo = this.todos.find(
+      (todo) => todo.id === updateStatusDto.id,
+    );
+    if (!targetTodo) {
+      throw new NotFoundException();
+    }
+    const newTodo = {
+      ...targetTodo,
+      status: updateStatusDto.status,
+      updatedAt: new Date(),
+    };
+    this.todos = this.todos.map((todo) =>
+      todo.id === newTodo.id ? newTodo : todo,
+    );
     return newTodo;
   }
 }
